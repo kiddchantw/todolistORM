1fac4834abeec392f7675cd70658556158fbdb9a
K88 <kiddchantw@gmail.com>
2020-07-20 19:55:45 +0800

    feat:ignore

--- config/DBsetting.php
+++ config/DBsetting.php
@@ -1,29 +1,26 @@
 <?php
+
 namespace modelK;
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 
 class DBsetting
 {
-
-	function __construct()
-	{
-		$capsule = new Capsule;
-
-	$arrayCondition = [
-			'driver'    => 'mysql',
-    'host'      => '127.0.0.1',   // //localhost
-    'database'  => 'dbtest001',
-    'username'  => 'root',
-    'password'  => '1234',
-    'charset'   => 'utf8',
-    'collation' => 'utf8_unicode_ci',
-    'prefix'    => '', ];
-    $capsule->addConnection($arrayCondition);
-    $capsule->setAsGlobal();
-    $capsule->bootEloquent();
-	}
-
-
+    function __construct()
+    {
+        $capsule = new Capsule;
+        $arrayCondition = [
+            'driver'    => 'mysql',
+            'host'      => '127.0.0.1',   // //localhost
+            'database'  => 'dbtest001',
+            'username'  => 'root',
+            'password'  => '1234',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ];
+        $capsule->addConnection($arrayCondition);
+        $capsule->setAsGlobal();
+        $capsule->bootEloquent();
+    }
 }
-?>
